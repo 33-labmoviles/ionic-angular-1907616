08fc33948fb36a4b70a7375c13f9be23077b7033
Andrés <abrisenocristerna@gmail.com>
2022-05-14 15:36:25 -0500

    Practica 10 Android

--- src/app/lista-detalle/lista-detalle.component.ts
+++ src/app/lista-detalle/lista-detalle.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { ActionSheetController } from '@ionic/angular';
 
+import { DatabaseService } from '../database.service';
+
 @Component({
   selector: 'app-lista-detalle',
   templateUrl: './lista-detalle.component.html',
@@ -10,104 +12,22 @@ import { ActionSheetController } from '@ionic/angular';
 export class ListaDetalleComponent implements OnInit {
 
 
-  constructor(private ruta :ActivatedRoute, public actionSheetC: ActionSheetController) {}
+  constructor(
+    private ruta :ActivatedRoute,
+    private db: DatabaseService,
+    public actionSheetC: ActionSheetController) {}
 
     ngOnInit() {
+      console.log(this.matricula)
       this.obtenerDetalleLista(this.matricula);
     }
 
-    listas = [
-      {
-        "nombre": "Andres",
-        "apellido": "Briseno",
-        "matricula": "ABC123",
-        "imagen" : "https://vignette.wikia.nocookie.net/lossimpson/images/5/55/Desconocido.png/revision/latest?cb=20150614004847&path-prefix=es"
-      },
-      {
-        "nombre": "Alejandro",
-        "apellido": "Cristerna",
-        "matricula": "ABC123",
-        "imagen" : "https://vignette.wikia.nocookie.net/lossimpson/images/5/55/Desconocido.png/revision/latest?cb=20150614004847&path-prefix=es"
-      },
-      {
-        "nombre": "Peter",
-        "apellido": "Parker 1",
-        "matricula": "ABC123",
-        "imagen" : "https://vignette.wikia.nocookie.net/lossimpson/images/5/55/Desconocido.png/revision/latest?cb=20150614004847&path-prefix=es"
-      },
-      {
-        "nombre": "Peter",
-        "apellido": "Parker 2",
-        "matricula": "ABC123",
-        "imagen" : "https://vignette.wikia.nocookie.net/lossimpson/images/5/55/Desconocido.png/revision/latest?cb=20150614004847&path-prefix=es"
-      },
-      {
-        "nombre": "Peter",
-        "apellido": "Parker 3",
-        "matricula": "ABC123",
-        "imagen" : "https://vignette.wikia.nocookie.net/lossimpson/images/5/55/Desconocido.png/revision/latest?cb=20150614004847&path-prefix=es"
-      },
-      {
-        "nombre": "Tobey",
-        "apellido": "Maguire",
-        "matricula": "ABC123",
-        "imagen" : "https://vignette.wikia.nocookie.net/lossimpson/images/5/55/Desconocido.png/revision/latest?cb=20150614004847&path-prefix=es"
-      },
-      {
-        "nombre": "Andrew",
-        "apellido": "Garfield",
-        "matricula": "ABC123", 
-        "imagen" : "https://vignette.wikia.nocookie.net/lossimpson/images/5/55/Desconocido.png/revision/latest?cb=20150614004847&path-prefix=es"
-      },
-        {
-        "nombre": "Tom",
-        "apellido": "Holland",
-        "matricula": "ABC123",
-        "imagen" : "https://vignette.wikia.nocookie.net/lossimpson/images/5/55/Desconocido.png/revision/latest?cb=20150614004847&path-prefix=es"
-      },
-       {
-        "nombre": "Willem",
-        "apellido": "Dafoe",
-        "matricula": "ABC123",
-        "imagen" : "https://vignette.wikia.nocookie.net/lossimpson/images/5/55/Desconocido.png/revision/latest?cb=20150614004847&path-prefix=es"
-      },
-      {
-        "nombre": "Alfred",
-        "apellido": "Molina",
-        "matricula": "ABC123",
-        "imagen" : "https://vignette.wikia.nocookie.net/lossimpson/images/5/55/Desconocido.png/revision/latest?cb=20150614004847&path-prefix=es"
-      },
-      {
-        "nombre": "Benedict",
-        "apellido": "Cumberbatch",
-        "matricula": "ABC123",
-        "imagen" : "https://vignette.wikia.nocookie.net/lossimpson/images/5/55/Desconocido.png/revision/latest?cb=20150614004847&path-prefix=es"
-      },
-      {
-        "nombre": "Jaime",
-        "apellido": "Foxx",
-        "matricula": "ABC123",
-        "imagen" : "https://vignette.wikia.nocookie.net/lossimpson/images/5/55/Desconocido.png/revision/latest?cb=20150614004847&path-prefix=es"
-      },
-      {
-        "nombre": "Charlie",
-        "apellido": "Cox",
-        "matricula": "ABC123",
-        "imagen" : "https://vignette.wikia.nocookie.net/lossimpson/images/5/55/Desconocido.png/revision/latest?cb=20150614004847&path-prefix=es"
-      }
-    ];
-   
     listaDetalle: any={}
-    matricula: string = this.ruta.snapshot.params.id
+    matricula: string = this.ruta.snapshot.params.matricula;
+   
     obtenerDetalleLista(matricula: String): any{
-      console.log(matricula);
+      this.db.detalleLista(matricula)
 
-for (let i = 0; i < this.listas.length; i++) {
-  if (matricula == this.listas[i].matricula) {
-    this.listaDetalle = this.listas[i];
-  }
-  
-}
 
       return this.listaDetalle;
     }
